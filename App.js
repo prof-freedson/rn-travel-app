23cb7f80b5212e5f039a958b42b307aaa5e1775e
prof-freedson <freedson.prof@gmail.com>
2023-08-16 15:04:07 -0300

    Configuração de navegação

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="" screenOptions={{headerShown: 'false'}}>
+        <Stack.Screen name='splash' component={Splash} />
+        <Stack.Screen name='login' component={Login} />
+        <Stack.Screen name='cadastro' component={Cadastro} />
+        <Stack.Screen name='inicio' component={Inicio} />
+        <Stack.Screen name='reservas' component={Reservas} />
+        <Stack.Screen name='viagens' component={Viagens} />
+        <Stack.Screen name='sobre' component={Sobre} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
