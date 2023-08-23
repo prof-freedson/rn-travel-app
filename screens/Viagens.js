5c5a1ab28ebe189ac6b10145c5729751c8748a6e
prof-freedson <freedson.prof@gmail.com>
2023-08-23 15:59:34 -0300

    Elemento cartaoViagem da tela iagens finalizado

--- screens/Viagens.js
+++ screens/Viagens.js
@@ -1,7 +1,8 @@
-import { StyleSheet, View, Text, Pressable } from "react-native";
+import { StyleSheet, View, Text, Pressable, ScrollView } from "react-native";
 import { useFonts, PlayfairDisplay_600SemiBold as playfair } from "@expo-google-fonts/playfair-display";
+import { MaterialIcons} from '@expo/vector-icons/'
 
-export default function Viagens({ navigation }){
+export default function Viagens({ navigation }) {
     let [fontsLoaded, fontError] = useFonts({
         playfair
     });
@@ -9,12 +10,119 @@ export default function Viagens({ navigation }){
     if (!fontsLoaded && !fontError) {
         return null;
     }
-    return(
+    return (
         <View style={styles.container}>
             <Text style={{ fontSize: 30, color: '#1a4252', fontWeight: 'bold', fontFamily: 'playfair' }}>Minhas viagens</Text>
-            <View>
-                <Text>Nenhuma viagem marcada</Text>
-            </View>
+            <ScrollView style={{ height: '100%' }}>
+                <View style={styles.cartaoViagem}>
+                    <View style={styles.cartaoViagemSup}>
+                        <View style={styles.cartaoViagemDataHora}>
+                            <View style={styles.cartaoViagemData}>
+                                <Text>Sábado</Text>
+                                <Text>31/12/2022</Text>
+                            </View>
+                            <Text style={{fontSize: 18}}>22h00min</Text>
+                        </View>
+                        <Text style={{fontSize: 40, fontWeight: 'bold', color: '#0c2c3a'}}>J-1362073FCR</Text>
+                    </View>
+                    <View style={styles.cartaoViagemInf}>
+                        <MaterialIcons style={{marginRight: 10}} name="push-pin" color={'#f9d04c'} size={25}/>
+                        <Text style={{color: 'white'}}>
+                            Aeroporto de Congonhas (CGH){'\n'}São Paulo, SP
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.cartaoViagem}>
+                    <View style={styles.cartaoViagemSup}>
+                        <View style={styles.cartaoViagemDataHora}>
+                            <View style={styles.cartaoViagemData}>
+                                <Text>Sábado</Text>
+                                <Text>31/12/2022</Text>
+                            </View>
+                            <Text style={{fontSize: 18}}>22h00min</Text>
+                        </View>
+                        <Text style={{fontSize: 40, fontWeight: 'bold', color: '#0c2c3a'}}>J-1362073FCR</Text>
+                    </View>
+                    <View style={styles.cartaoViagemInf}>
+                        <MaterialIcons style={{marginRight: 10}} name="push-pin" color={'#f9d04c'} size={25}/>
+                        <Text style={{color: 'white'}}>
+                            Aeroporto de Congonhas (CGH){'\n'}São Paulo, SP
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.cartaoViagem}>
+                    <View style={styles.cartaoViagemSup}>
+                        <View style={styles.cartaoViagemDataHora}>
+                            <View style={styles.cartaoViagemData}>
+                                <Text>Sábado</Text>
+                                <Text>31/12/2022</Text>
+                            </View>
+                            <Text style={{fontSize: 18}}>22h00min</Text>
+                        </View>
+                        <Text style={{fontSize: 40, fontWeight: 'bold', color: '#0c2c3a'}}>J-1362073FCR</Text>
+                    </View>
+                    <View style={styles.cartaoViagemInf}>
+                        <MaterialIcons style={{marginRight: 10}} name="push-pin" color={'#f9d04c'} size={25}/>
+                        <Text style={{color: 'white'}}>
+                            Aeroporto de Congonhas (CGH){'\n'}São Paulo, SP
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.cartaoViagem}>
+                    <View style={styles.cartaoViagemSup}>
+                        <View style={styles.cartaoViagemDataHora}>
+                            <View style={styles.cartaoViagemData}>
+                                <Text>Sábado</Text>
+                                <Text>31/12/2022</Text>
+                            </View>
+                            <Text style={{fontSize: 18}}>22h00min</Text>
+                        </View>
+                        <Text style={{fontSize: 40, fontWeight: 'bold', color: '#0c2c3a'}}>J-1362073FCR</Text>
+                    </View>
+                    <View style={styles.cartaoViagemInf}>
+                        <MaterialIcons style={{marginRight: 10}} name="push-pin" color={'#f9d04c'} size={25}/>
+                        <Text style={{color: 'white'}}>
+                            Aeroporto de Congonhas (CGH){'\n'}São Paulo, SP
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.cartaoViagem}>
+                    <View style={styles.cartaoViagemSup}>
+                        <View style={styles.cartaoViagemDataHora}>
+                            <View style={styles.cartaoViagemData}>
+                                <Text>Sábado</Text>
+                                <Text>31/12/2022</Text>
+                            </View>
+                            <Text style={{fontSize: 18}}>22h00min</Text>
+                        </View>
+                        <Text style={{fontSize: 40, fontWeight: 'bold', color: '#0c2c3a'}}>J-1362073FCR</Text>
+                    </View>
+                    <View style={styles.cartaoViagemInf}>
+                        <MaterialIcons style={{marginRight: 10}} name="push-pin" color={'#f9d04c'} size={25}/>
+                        <Text style={{color: 'white'}}>
+                            Aeroporto de Congonhas (CGH){'\n'}São Paulo, SP
+                        </Text>
+                    </View>
+                </View>
+                <View style={styles.cartaoViagem}>
+                    <View style={styles.cartaoViagemSup}>
+                        <View style={styles.cartaoViagemDataHora}>
+                            <View style={styles.cartaoViagemData}>
+                                <Text>Sábado</Text>
+                                <Text>31/12/2022</Text>
+                            </View>
+                            <Text style={{fontSize: 18}}>22h00min</Text>
+                        </View>
+                        <Text style={{fontSize: 40, fontWeight: 'bold', color: '#0c2c3a'}}>J-1362073FCR</Text>
+                    </View>
+                    <View style={styles.cartaoViagemInf}>
+                        <MaterialIcons style={{marginRight: 10}} name="push-pin" color={'#f9d04c'} size={25}/>
+                        <Text style={{color: 'white'}}>
+                            Aeroporto de Congonhas (CGH){'\n'}São Paulo, SP
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
             <Pressable style={styles.botaoViagem} onPress={() => navigation.navigate('criarviagem')}>
                 <Text>AGENDAR NOVA VIAGEM</Text>
             </Pressable>
@@ -37,6 +145,34 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         paddingHorizontal: 20,
-        borderRadius: 5
+        borderRadius: 5,
+        marginTop: 10
+    },
+    cartaoViagem:{
+        marginVertical: 10
+    },
+    cartaoViagemSup: {
+        paddingVertical: 15,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        backgroundColor: 'white',
+        width: 350,
+        alignItems: 'center',
+    },
+    cartaoViagemDataHora: {
+        flexDirection: 'row',
+        paddingHorizontal: 30,
+        width: 350,
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    cartaoViagemInf: {
+        flexDirection: 'row',
+        backgroundColor: '#0c2c3a',
+        paddingVertical: 10,
+        paddingHorizontal: 30,
+        alignItems: 'flex-start',
+        borderBottomLeftRadius: 10,
+        borderBottomRightRadius: 10,
     }
 })
